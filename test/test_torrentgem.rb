3b30b7998e5e8d40cd81dc996c22cde8d56be037
OHASHI Hideya <ohachige@gmail.com>
2011-04-13 00:09:42 +0900

    dummy test

--- test/test_torrentgem.rb
+++ test/test_torrentgem.rb
@@ -2,6 +2,6 @@ require 'helper'
 
 class TestTorrentgem < Test::Unit::TestCase
   should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+    true
   end
 end
